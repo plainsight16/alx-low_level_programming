fd7c1d07dc0f4be620f708caa892b4a4f96aafc8
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-12 03:15:44 -0800

    t-4 done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 # include <stdio.h>
 
+/*
+ * main - Entry Point
+ * puts - sends string to stdout
+ * return(0) - siginifies success
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
