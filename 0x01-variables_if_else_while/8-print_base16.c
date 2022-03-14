710351c38b4b8d695e9015cc4cbaf7683fa673f6
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-14 01:44:47 -0700

    t-8 done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+	* main - Entry point
+	* Description: print numbers in base16 lowercase
+	* Return: 0
+*/
+int main(void)
+{
+		int num = 0;
+		char lower = 'a';
+		
+		while (num < 10)
+		{
+			putchar(num++ + '0');
+		}
+		while (lower < 'g')
+		{
+			putchar(lower++);
+		}
+		putchar('\n');
+		return (0);
+}
+
