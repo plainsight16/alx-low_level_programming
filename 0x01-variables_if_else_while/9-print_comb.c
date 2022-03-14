929c1967d15c5a13e0b92b51ef1b2ee63aeade27
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-14 03:13:43 -0700

    t-9 done-3

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,13 @@ int main(void)
 
 	while (num < 10)
 	{
-		putchar(num++ + '0');
+		putchar(num + '0');
 		if (num < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		num++;
 	}
 	putchar('\n');
 	return (0);
