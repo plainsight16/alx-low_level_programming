0e4783883c3f71721c33356fc0905834eae2bf1a
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-14 13:54:01 -0700

    t-11 done

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+	* main - Entry point
+	* Description: prints all possible combinations of 3 digits with commas and a space seperating them
+	* Return: 0
+*/
+int main(void)
+{
+	int i = 0;
+	int j, k;
+
+	while (i < 1000)
+	{
+		j = (i / 10) % 10; /* Tens */
+		k = i % 10; /* Units */
+
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(k + '0');
+
+		if (i < 800)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	}
+	putchar('\n');
+}
