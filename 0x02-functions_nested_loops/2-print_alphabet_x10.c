b4161fc0d5d1dab1119cf2f20ac38d03f7cb684f
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-15 16:01:06 -0700

    t-2 done(0)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * print_alphabet_x10 - prints alphabets in lowercase 10x
+*/
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		print_alphabet();
+	}
+}
