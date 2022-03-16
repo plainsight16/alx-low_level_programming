254b5ac6d477a72e99867dc7e59393a7c9f3dc9a
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-16 07:55:44 -0700

    t-9 done(1)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,16 +5,22 @@
 void times_table(void)
 {
 	int count = 0;
+	int multiplier;
+	int result;
 
 	while (count < 10)
 	{
-		int multiplier = 0;
+		multiplier = 0;
 
 		while (multiplier < 10)
 		{
-			_putchar((count * multiplier) + '0');
-			_putchar(',');
-			_putchar(' ');
+			result = count * multiplier;
+			_putchar(result + '0');
+			if (multiplier < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			multiplier++;
 		}
 		_putchar('\n');
