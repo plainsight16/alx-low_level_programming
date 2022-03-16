54d872d80ccb6521444d75d13eff70870f9fa629
Timilehin Bakare <64688744+plainsight16@users.noreply.github.com>
2022-03-16 22:35:12 +0100

    done(1)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,20 +5,23 @@
 void times_table(void)
 {
 	int count = 0;
-	int multiplier;
-	int result;
+	int multiplier, result;
 
 	while (count < 10)
 	{
 		multiplier = 0;
-
 		while (multiplier < 10)
 		{
-			if (multiplier > 0)
-				_putchar(' ');
 			result = count * multiplier;
-			if (result >= 0 && result < 10)
+			if (multiplier == 0)
+			{
 				_putchar(result + '0');
+			}
+			else if (result >= 0 && result < 10)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
 			else if (result >= 10 && result < 100)
 			{
 				_putchar((result / 10) + '0');
