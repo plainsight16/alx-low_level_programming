e848bc386ad909baa3732b3fd660a0c982c47a30
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-21 07:52:57 -0700

    t-3 done(0)

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _puts - prints a string, followed by a new line to stdout
+  * @str: char pointer
+*/
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
