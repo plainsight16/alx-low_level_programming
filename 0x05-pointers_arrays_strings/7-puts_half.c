64c2a3f4008be507344329aa157108702f72d0c1
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-21 14:40:36 -0700

    t-7 done(0)

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * puts_half - prints half of a string
+  * @str: String param
+*/
+void puts_half(char *s)
+{
+	int length = _strlen(*s);
+	int half_length = length / 2;
+
+	while (half_length <= length)
+	{
+		_putchar(*(s + half_length));
+		half_length++;
+	}
+	_putchar('\n');
+}
