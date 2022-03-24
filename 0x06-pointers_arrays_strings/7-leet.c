8833f93dad379f4dca543dba8b3942b99da80297
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-24 09:48:58 -0700

    t-7 done(0)

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  * leet - encodes strings
+  * @s: string pointer
+  * Return: string pointer
+*/
+char *leet(char *s)
+{
+	int i = 0;
+	char key[] = {'A', 'E', 'I', 'O', 'U'};
+	char values[] = {4, 3, 0, 7, 1};
+
+	while (s[i] != '\0')
+	{
+		for (int j = 0; j < sizeof(key); j++)
+		{
+			if (s[i] == key[j] || s[i] == key[j] + 32)
+			{
+				s[i] = values[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
