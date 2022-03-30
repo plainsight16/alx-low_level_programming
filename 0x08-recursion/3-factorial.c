c25d1b86c1288caf64969a8d1f18c349ba12ee8e
Timilehin Bakare <timmybaks2002@gmail.com>
2022-03-30 09:17:59 -0700

    t-3 done tries: 0

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * factorial - returns factorial
+  * @n: give number
+  * Return: factorial
+*/
+int factorial(int n)
+{
+	if (n < 1)
+		return (1);
+	return (n * factorial(--n));
+}
