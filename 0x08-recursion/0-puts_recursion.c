0bf3364853d052e214bd1a06806825a62b9c1c0e
Timilehin Bakare <64688744+plainsight16@users.noreply.github.com>
2022-03-30 15:50:54 +0100

    t-0 done(3)

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,12 @@
 */
 void _puts_recursion(char *s)
 {
-	if (!*s)
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s++);
 	}
-	_putchar(*s);
-	_puts_recursion(s++);
+	else
+		_putchar('\n');
+	
 }
