a51ece91b0bf07e3919df0080d331bb5d352b6b1
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-02 07:40:17 -0700

    t-1 done, tries: 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+
+/**
+  * main - main func
+  * @argc: arg count
+  * @argv: arg vector
+  *
+  * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	UNUSED(argv);
+	printf("%d\n", argc);
+	return (0);
+}
