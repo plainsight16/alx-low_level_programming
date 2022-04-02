908adde857f46b8fcafd433d86209b1fe3c1b511
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-02 10:42:26 -0700

    t-4 done, tries: 1

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - main func
+  * @argc: arg count
+  * @argv: arg vector
+  *
+  * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 0)
+		printf("%d\n", 0);
+
+	for (i = 0; i < argc; i++)
+	{
+		if (!isdigit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
