dd417d58825c034715ce40aec201fcefe3dbe080
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-02 07:29:49 -0700

    t-0 done, tries: 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 /**
   * main - main func
   * @argc: args count
@@ -10,5 +11,7 @@
 
 int main(int argc, char *argv[])
 {
+	UNUSED(argc);
 	printf("%s\n", argv[0]);
+	return (0);
 }
