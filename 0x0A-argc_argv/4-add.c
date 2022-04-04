c6078368ef6b6bece901376eb56a7f8605ee6389
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-04 03:42:46 -0700

    t-4 done, tries: 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#define UNUSED(x) (void)(x)
+
+/**
+  * StringCheck - checks if str
+  * @s: string pointer
+  * Return: 0 if false, 1 if true
+*/
+
+int StringCheck(char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\0'; i++)
+	{
+		if (s[i] < 48 || s[i] > 57)
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
 
 /**
   * main - main func
@@ -15,7 +36,7 @@ int main(int argc, char *argv[])
 {
 	int i, sum = 0;
 
-	if (argc == 0)
+	if (argc == 1)
 	{
 		printf("%d\n", 0);
 		return (0);
@@ -23,14 +44,16 @@ int main(int argc, char *argv[])
 
 	for (i = 0; i < argc; i++)
 	{
-		if (!isdigit(argv[i]))
+		if (StringCheck(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += atoi(argv[i]);
 	}
-
 	printf("%d\n", sum);
 	return (0);
 }
