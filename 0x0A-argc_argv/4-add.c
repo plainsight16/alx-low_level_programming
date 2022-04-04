8a78605a6fd22aa06743238dd9a987283af36bce
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-04 02:21:58 -0700

    t-4 done, tries: 2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
   * main - main func
@@ -15,7 +16,10 @@ int main(int argc, char *argv[])
 	int i, sum = 0;
 
 	if (argc == 0)
+	{
 		printf("%d\n", 0);
+		return (0);
+	}
 
 	for (i = 0; i < argc; i++)
 	{
