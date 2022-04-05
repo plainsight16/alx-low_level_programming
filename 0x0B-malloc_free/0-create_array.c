7ade0e0490d7b67a686764793b8a072ed5909090
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-05 02:56:51 -0700

    t-0 done, tries: 4

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,8 +12,8 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *array = malloc((size_of(char)) * (size + 1));
-	int i = 0;
+	char *array = malloc((sizeof(char)) * (size + 1));
+	unsigned int i = 0;
 
 	if (size <= 0)
 		return (NULL);
