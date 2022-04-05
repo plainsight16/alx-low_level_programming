1627504838d59c430b506b0d648878456ee8171a
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-05 00:27:02 -0700

    t-5 done, tries: 1

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+  * main - main func
+  * @argc: arg counter
+  * @argv: arg vector
+  *
+  * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int coins = 0;
+	int money;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	money = atoi(argv[1]);
+
+	if (money < 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+
+	while (money > 0)
+	{
+		if (money % 25 == 0)
+			money -= 25;
+		else if (money % 10 == 0)
+			money -= 10;
+		else if (money % 5 == 0)
+			money -= 5;
+		else if (money % 2 == 0)
+			money -= 2;
+		else if (money % 1 == 0)
+			money -= 1;
+		else
+			money--;
+		coins++;
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
