975ff31975c74f4c70a5dc4155a54e36fe2b6fc6
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-08 02:05:21 -0700

    t-0 done, tries: 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * malloc_checked - checks malloc
+  * @b: int param
+*/
+void *malloc_checker(unsigned int b)
+{
+	void *t;
+
+	t = malloc(b);
+	if (t == NULL)
+		exit(98);
+	return (t);
+}
