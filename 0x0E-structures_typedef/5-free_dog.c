fd4ee4ed0c11ca918e5e344dc71d11907ece061e
Timilehin Bakare <timmybaks2002@gmail.com>
2022-04-12 17:24:17 -0700

    t-5 done

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * free_dog - frees dog
+  * @d: dog
+*/
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
